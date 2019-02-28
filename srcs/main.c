34e2ed75415bc9f66f5de647a11af148be344bad
Alan Barnett <alanbarnett328@gmail.com>
2019-02-28 03:08:05 -0800

    Make function for reading /dev/random

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 01:21:46 by alan              #+#    #+#             */
-/*   Updated: 2019/02/28 02:50:07 by alan             ###   ########.fr       */
+/*   Updated: 2019/02/28 03:06:21 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,25 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int		main(int argc, char **argv)
+int		read_random(t_dice *dice)
 {
 	int		fd;
 	int		size_read;
+
+	fd = open("/dev/random", O_RDONLY);
+	size_read = read(fd, dice->nums, sizeof(int) * dice->how_many);
+	if ((unsigned long)size_read < (sizeof(int) * dice->how_many))
+	{
+		ft_printfd(2, "read error, try again later (%lu/%lu)\n",
+				(unsigned long)size_read, (sizeof(int) * dice->how_many));
+		return (-1);
+	}
+	close(fd);
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
 	int		rolls;
 	t_dice	*dice;
 
@@ -34,17 +49,12 @@ int		main(int argc, char **argv)
 		return (1);
 	}
 	dice = init_dice(rolls, ft_atoi(argv[2]));
-	fd = open("/dev/random", O_RDONLY);
-	size_read = read(fd, dice->nums, sizeof(int) * dice->how_many);
-	if ((unsigned long)size_read < (sizeof(int) * dice->how_many))
+	if (read_random(dice) == -1)
 	{
-		ft_printfd(2, "read error, try again later (%lu/%lu)\n",
-				(unsigned long)size_read, (sizeof(int) * dice->how_many));
 		return (1);
 	}
 	roll_dice(dice);
 	delete_dice(dice);
 	ft_putchar('\n');
-	close(fd);
 	return (0);
 }
