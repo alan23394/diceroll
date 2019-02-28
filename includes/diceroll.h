d40420eafbb325fe166e19dc42cc37bed967bb2c
Alan Barnett <alanbarnett328@gmail.com>
2019-02-28 03:18:26 -0800

    Update documentation

--- includes/diceroll.h
+++ includes/diceroll.h
@@ -6,23 +6,25 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 00:57:23 by alan              #+#    #+#             */
-/*   Updated: 2019/02/28 02:39:04 by alan             ###   ########.fr       */
+/*   Updated: 2019/02/28 03:18:14 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 **	**************************       USAGE        ***********************{{{
-**	*   The nums variable should be an array of amount MAX_NUMS numbers    *
-**	*   of type unsigned int. It is allocated in the init_dice function,   *
-**	*   so you just have to fill it.                                       *
+**	*   The nums variable should be an array of less than MAX_NUMS         *
+**	*   numbers of type unsigned int. It is allocated in the init_dice     *
+**	*   function to size how_many, so you just have to fill it.            *
+**	*   The init function will return (0) if you give it a value for       *
+**	*   how_many that is larger than MAX_NUMS.                             *
 **	*                                                                      *
 **	*   When grabbing a number from nums, you must index it with the       *
 **	*   nums_counter variable.                                             *
-**	*   It should NOT be indexed if nums_counter is >= MAX_NUMS!           *
+**	*   It should NOT be indexed if nums_counter is >= how_many!           *
 **	*   Remember to incrememt nums_counter each time you take a number     *
 **	*   from nums, to keep track of how many numbers have been taken.      *
-**	*   Note: this is implemented in the roll function, which returns      *
-**	*   the result of the index.                                           *
+**	*   Note: this all is implemented in the roll function, which          *
+**	*   returns the result of the index.                                   *
 **	*********************************************************************}}}
 */
 
