41b9922981c6e6431a05e03f399d5733548cecb4
Alan Barnett <alanbarnett328@gmail.com>
2019-02-28 03:53:25 -0800

    Make header for random sources
    This is for different functions that get random numbers to fill the int
    pointer in a dice struct. So far, the only one opens and reads from
    /dev/random, but eventually I want to make different ones, like one to
    get random numbers from time or something like that.

---
+++ includes/random_sources.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   random_sources.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/28 03:37:58 by alan              #+#    #+#             */
+/*   Updated: 2019/02/28 03:49:48 by alan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RANDOM_SOURCES_H
+# define RANDOM_SOURCES_H
+
+typedef struct	s_dice t_dice;
+
+int				read_dev_random(t_dice *dice);
+
+#endif
