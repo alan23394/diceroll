92a41b3865c8191f90e06384c90addeeaa7c92d1
Alan Barnett <alanbarnett328@gmail.com>
2019-02-28 03:52:41 -0800

    Move function out of main

--- srcs/main.c
+++ srcs/main.c
@@ -6,55 +6,29 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 01:21:46 by alan              #+#    #+#             */
-/*   Updated: 2019/02/28 03:06:21 by alan             ###   ########.fr       */
+/*   Updated: 2019/02/28 03:49:05 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "diceroll.h"
-#include <unistd.h>
-#include <fcntl.h>
-
-int		read_random(t_dice *dice)
-{
-	int		fd;
-	int		size_read;
-
-	fd = open("/dev/random", O_RDONLY);
-	size_read = read(fd, dice->nums, sizeof(int) * dice->how_many);
-	if ((unsigned long)size_read < (sizeof(int) * dice->how_many))
-	{
-		ft_printfd(2, "read error, try again later (%lu/%lu)\n",
-				(unsigned long)size_read, (sizeof(int) * dice->how_many));
-		return (-1);
-	}
-	close(fd);
-	return (0);
-}
+#include "random_sources.h"
 
 int		main(int argc, char **argv)
 {
-	int		rolls;
-	t_dice	*dice;
+	int		amount;
 
 	if (argc != 3)
 	{
 		ft_printfd(2, "usage: diceroll [number of die] [how many sides]\n");
 		return (1);
 	}
-	rolls = ft_atoi(argv[1]);
-	if (rolls > MAX_NUMS)
+	amount = ft_atoi(argv[1]);
+	if (amount > MAX_NUMS)
 	{
 		ft_printfd(2, "no more than %d dice at a time\n", MAX_NUMS);
 		return (1);
 	}
-	dice = init_dice(rolls, ft_atoi(argv[2]));
-	if (read_random(dice) == -1)
-	{
-		return (1);
-	}
-	roll_dice(dice);
-	delete_dice(dice);
-	ft_putchar('\n');
+	diceroll(amount, ft_atoi(argv[2]), read_dev_random);
 	return (0);
 }
